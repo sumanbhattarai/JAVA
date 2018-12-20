b9f4414f7410b7603a4d067e767eb65ecf1a6930
Suman Bhattarai <33854515+sumanbhattarai@users.noreply.github.com>
2018-12-20 07:22:26 +0545

    Create boxx.java

---
+++ OOP/Constructor/boxx.java
@@ -0,0 +1,33 @@
+class boxx
+{
+  double height , width , length ;
+
+//default constructor
+  boxx()
+  {
+    height = 3.0 ;
+    width = 9.0 ;
+    length = 1.0 ;
+  }
+
+
+//parameterized constructors
+  boxx(double height , double width , double length )
+  {
+    this.height = height ;
+    this.width = width ;
+    this.length = length ;
+  }
+
+  //void initiateVariable()
+  //{
+  //  height = 2.0 ;
+  //    width = 3.0 ;
+  //  length = 4.0 ;
+  //}
+
+  void volume()
+  {
+    System.out.println(height*width*length) ;
+  }
+}
