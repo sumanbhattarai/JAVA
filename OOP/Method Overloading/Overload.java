98c5837718f268e5c980a23181db6613bed5f151
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-01-15 15:26:29 +0545

    Method Overloading

---
+++ OOP/Method Overloading/Overload.java
@@ -0,0 +1,27 @@
+
+
+class Example
+{
+    public int sum(int x , int y)
+    {
+        int result = x + y ;
+        return result ; 
+    }
+
+    public int sum (int x , int y , int z)
+    {
+        int result = x + y + z ;
+        return result ;
+    }
+}
+
+class Overload
+{
+    public static void main(String[] args) {
+        Example obj = new Example();
+        System.out.println(obj.sum(2,3));
+        System.out.println(obj.sum(2,3,4));
+
+        
+    }
+}
