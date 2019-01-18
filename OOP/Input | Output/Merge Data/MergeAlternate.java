0414887e2a44b195413dd6124e8a2fb4254c1346
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-01-18 08:02:56 +0545

    Merge File1 and File2 into File4 by alternative line

---
+++ OOP/Input | Output/Merge Data/MergeAlternate.java
@@ -0,0 +1,26 @@
+
+import java.io.* ;
+
+class MergeAlternate
+{
+    public static void main(String[] args) throws IOException
+    {
+        PrintWriter pw = new PrintWriter("file4.txt") ;
+        BufferedReader br1 = new BufferedReader(new FileReader("file1.txt")) ;
+        BufferedReader br2 = new BufferedReader(new FileReader("file2.txt")) ;
+        String Line1 = br1.readLine() ;
+        String Line2 = br2.readLine() ;
+        while(Line1!=null || Line2!=null)
+        {
+            pw.println(Line1) ;
+            pw.println(Line2) ;
+            Line1 = br1.readLine() ;
+            Line2 = br2.readLine() ;
+        }
+        pw.flush();
+        pw.close();
+        
+
+        
+    }
+}
