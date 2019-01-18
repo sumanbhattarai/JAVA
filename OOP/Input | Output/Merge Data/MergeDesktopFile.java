c8f064bdf8b5d6cfe0b835180f01f86dea53c524
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-01-18 09:58:09 +0545

    Merge all the Desktop .txt file into output.txt

---
+++ OOP/Input | Output/Merge Data/MergeDesktopFile.java
@@ -0,0 +1,32 @@
+
+import java.io.* ;
+
+
+class MergeDesktopFile
+{
+    public static void main(String[] args) throws IOException
+    {
+        PrintWriter pw = new PrintWriter("/Users/macbookpro/Desktop/Output.txt") ;
+        File file = new File("/Users/macbookpro/Desktop") ;
+        File[] files = file.listFiles();
+        for(File f: files){
+            if (f.getName().endsWith((".txt"))) {
+                
+                String location = "/Users/macbookpro/Desktop/" + f.getName() ;
+                BufferedReader br = new BufferedReader(new FileReader(location)) ;
+                String line = br.readLine() ;
+                while(line!= null)
+                {
+                    pw.println(line) ;
+                    line = br.readLine() ;
+
+                }
+              }
+        }
+
+        pw.flush();
+        pw.close();
+        
+        
+    }
+}
