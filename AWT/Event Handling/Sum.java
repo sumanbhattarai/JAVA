725c64807b97c12827fc5231fac338c7bbca24e8
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-11 19:09:40 +0545

    Initial Commit

--- AWT/Event Handling/Sum.java
+++ AWT/Event Handling/Sum.java
@@ -1,28 +1,33 @@
 
+
 import java.awt.*;
 import java.awt.event.*;
 import java.applet.*;
 
 /*
-
-        < applet code = "Sum" height = "1000" width = "1000" >
-        </applet>
-
+	<applet code ="Sum" width = "1000" height = "1000" >
+</applet>
 */
 
 public class Sum extends Applet implements ActionListener
 {
-    public void init()
-    {
-        Label num1 = new Label ( "First Number " ) ; 
-        Label num2 = new Label ( "Second Number ") ; 
-        Label sum = new Label ( "SUM :  ") ;
-        Button sumButton = new Button ("Calculate Sum") ;
+	
+	public void init()
+	{
+        Label num1= new Label ( "First Number : "  , Label.CENTER) ;
+        Label num2 = new Label ( " Second Number : " , Label.CENTER ) ;
+        TextField Output =  new TextField(20) ;
+        Button calculateSum = new Button ("SUM") ;
         add(num1) ;
-        add(num2) ;
-        add(sum) ;
-        add(sumButton);
-        setLayout(false);
+        add(num2);
+        add(Output);
+        add(calculateSum);
+       Output.setEditable(false);
 
-    }
+	}
+
+	public void actionPerformed(ActionEvent ae)
+	{
+		
+	}
 }
