49d1023967aa9afd202e4a8431836449dcb6e7b0
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-20 11:01:56 +0545

    Main Method added

--- OOP/Access-Specifier/Movie.java
+++ OOP/Access-Specifier/Movie.java
@@ -11,4 +11,13 @@ class Movie
       System.out.println("Movie name is : " + name + "  and producer name is :  " + producer);
     }
 
+    public static void main(String[] args) {
+      Movie movie = new Movie() ;
+      // movie.name= "3 idiots" ;  //It doesn't work because tha name data in class Movie.java is private.
+      movie.Setname("ABC" , "XYZ");
+      movie.display();
+  
+  
+    }
+
 }
