0db64998b268c4353a5c2d9b28acd8e82d8f5f78
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-20 12:14:16 +0545

    Location re arranged

---
+++ OOP/Dynamic Method Dispatch/Dispatch.java
@@ -0,0 +1,40 @@
+class A
+{
+  void callMe()
+  {
+    System.out.println("Inside A") ;
+  }
+}
+
+class B extends A
+{
+  void callMe ()
+  {
+    System.out.println("Inside B") ;
+  }
+}
+
+class C extends A
+{
+  void callMe()
+  {
+    System.out.println("Inside C");
+  }
+}
+
+class Dispatch
+{
+  public static void main(String[] args)
+  {
+    A a = new A();
+    B b = new B();
+    C c = new C();
+    A r ; //reference of type A 
+    r = a ;//refers to A
+    r.callMe();
+    r = b ;
+    r.callMe();
+    r=c ;
+    r.callMe();
+  }
+}
--- OOP/Dynamic Method Dispatch/Readme.md
+++ OOP/Dynamic Method Dispatch/Readme.md
@@ -3,7 +3,6 @@ How JAVA implements run - time polymorphism ?
 
 - Overridden method allow java to support run-time polymorphism .
 
-- Dynamic method dispatch is the mechanism by which a call to an Overridden method is resolved at run time rather than compiler
-  time.
+- Dynamic method dispatch is the mechanism by which a call to an Overridden method is resolved at run time rather than compiler time.
 
 - The type of the object being referred to that determine which version of an overridden method will be executed.
