ab40b64204056dc3bd24c1f9e5487f11be64c528
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-20 12:44:45 +0545

    Updated

--- OOP/Method Overriding/StaticMethodOverriding.java
+++ OOP/Method Overriding/StaticMethodOverriding.java
@@ -1,4 +1,6 @@
+// We can declare static methods with same signature in subclass, but it is not considered overriding as there won’t be any run-time polymorphism.
 
+// If a derived class defines a static method with same signature as a static method in base class, the method in the derived class hides the method in the base class. 
 
 class Base
 {
