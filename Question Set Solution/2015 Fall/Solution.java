cb61185517363e45616f32c3cd714d5e1114705f
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-20 18:00:23 +0545

    Intial Commit

--- Question Set Solution/2015 Fall/Solution.java
+++ Question Set Solution/2015 Fall/Solution.java
@@ -1,52 +1,67 @@
+abstract class Book {
+    private String name;
+    private String author;
+    private int totalPage;
+    private String publisher;
 
-abstract class Book
-{
-     String name ;
-     String author ;
-     long totalpage ;
-     String publisher ;
-
-    public abstract String getBookType();
-
-
+    Book(String name, String author, int totalPage, String publisher) {
+        this.name = name;
+        this.author = author;
+        this.totalPage = totalPage;
+        this.publisher = publisher;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public int getTotalPage() {
+        return totalPage;
+    }
+    public String getPublisher() {
+        return publisher;
+    }
+    abstract void getBookType(); // Abstract Method
 }
 
-class Novel extends Book
-{
-    Novel( String name , String author , long totalpage , String publisher)
-    {
-        this.name = name ;
-        this.author = author ;
-        this.totalpage = totalpage ;
-        this.publisher = publisher ;
+class Novel extends Book {
 
+    Novel(String name, String author, int totalPage, String publisher) {
+        super(name, author, totalPage, publisher);
     }
 
-    public String getBookType()
-    {
-        return "Novel" ;
-    }
-
-
-}
-
-class Magazine extends Book
-{
     
-    Magazine( String name , String author , long totalpage , String publisher)
-    {
-        this.name = name ;
-        this.author = author ;
-        this.totalpage = totalpage ;
-        this.publisher = publisher ;
 
+    void getBookType() {
+        System.out.println("Book Type : "+this.getClass().getName());
+        System.out.println("Name : " + getName());
+        System.out.println("Author : " + getAuthor());
+        System.out.println("Total Page : " + getTotalPage());
+        System.out.println("Publisher: " + getPublisher());
+    }
+}
+
+class Magazine extends Book {
+
+    Magazine(String name, String author, int totalPage, String publisher) {
+        super(name, author, totalPage, publisher);
     }
 
-    public String getBookType()
-    {
-        return "Magazine" ;
+    void getBookType() {
+        System.out.println("Book Type : " + this.getClass().getName()); // This returns the class type
+        System.out.println("Name : " + getName());
+        System.out.println("Author : " + getAuthor());
+        System.out.println("Total Page : " + getTotalPage());
+        System.out.println("Publisher: " + getPublisher());
     }
+}
 
+class Seven{
+    public static void main(String[] args){
+        Magazine magazineOne = new Magazine("TIME", "Time Editorial", 61, "Time Publishers");
+        magazineOne.getBookType();
+    }
 }
 
 
