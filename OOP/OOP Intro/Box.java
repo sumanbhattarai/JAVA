c5550982dcd4e5624f0fada60ba7836ff5c9d90c
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-20 10:34:27 +0545

    Updated

--- OOP/OOP Intro/Box.java
+++ OOP/OOP Intro/Box.java
@@ -5,4 +5,16 @@ class Box
   {
     System.out.println("Volume is : " + height*width*depth) ;
   }
+
+  public static void main(String[] args) {
+    Box b1 = new Box() ;
+    Box b2 = new Box();
+    b2.width = 10.0 ;
+    b2.height = 40.0 ;
+    b2.depth = 20.0 ;
+    b1.volume();
+    b2.volume();
+
+
+ }
 }
