61ddf54e1568cf8cbc0b8b93a4acec9bf6030042
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-20 14:12:44 +0545

    Initial Commit

--- OOP/Interface/InterfaceDemo.java
+++ OOP/Interface/InterfaceDemo.java
@@ -1,16 +1,33 @@
-class InterfaceDemo
-{
-    public static void main(String[] args) 
-    {
-        Shape shape;
-        shape = new Circle();
-        shape.area();
-        shape.init();
-       
 
-        shape = new Rectangle();
-        shape.area();
-        shape.init();
+
+interface Web
+{
+    void frontend();
+    void backend();
+}
+
+
+class Suman implements Web 
+{
+    public void frontend()
+    {
+        System.out.println("FrontEnd Done.");
+    }
+    public void backend()
+    {
+        System.out.println("BackEnd Done.");
+    }
+    
+}
+
+
+
+class InterfaceDemo 
+{
+    public static void main(String[] args) {
+        Web obj = new Suman();
+        obj.frontend();
+        obj.backend();
         
     }
 }
