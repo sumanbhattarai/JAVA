3f37100bf89a4b3dd1eb1c3aaee14409efaed787
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-20 09:27:58 +0545

    Updated

--- Array/TwoDimensionalArray.java
+++ Array/TwoDimensionalArray.java
@@ -3,6 +3,9 @@ class TwoDimensionalArray
   public static void main(String args[])
   {
     int [][]arr = new int [2][] ;
+    int [][]suman = new int [3][] ;
     System.out.println(arr.getClass().getName()) ;
+    System.out.println(suman.getClass().getName()) ;
+
   }
 }
