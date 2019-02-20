2e0c94f570f92ceeade95fba029d02539026c544
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-20 09:10:22 +0545

    Updated

--- Types of Variable/PersonDemo.java
+++ Types of Variable/PersonDemo.java
@@ -6,6 +6,17 @@ class PersonDemo
         Person person2 = new Person();
         person2.enterRoom();
         System.out.println("Total entering is : " + Person.totalCount());
+        // We can also write this in another way .
+
+        System.out.println("Total entering is : " + Person.count);
+        System.out.println("Total entering is : " + person1.totalCount());
+        System.out.println("Total entering is : " + person2.totalCount());
+        System.out.println("Total entering is : " + person2.count);
+        System.out.println("Total entering is : " + person2.count);
+
+
+
+        
         
     }
 
