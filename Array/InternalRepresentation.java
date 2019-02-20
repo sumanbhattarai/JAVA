43dea53e7989ab0e4625c3193ece3291aea10124
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-20 09:19:58 +0545

    Fixed Errors

--- Array/InternalRepresentation.java
+++ Array/InternalRepresentation.java
@@ -1,21 +1,26 @@
+import java.lang.reflect.Array;
+
 class InternalRepresentation
 {
   public static void main(String [] args)
   {
-    int[] array ;
-    int[][] twoDArray ;
+    int[] array = new int[2];
+    int[][] twoDArray = new int[3][4] ;
     int integer ;
     double itsDouble ;
     short itsShort ;
     byte itsByte ;
     boolean itsBoolean ;
-    System.out.println(array.getClass().getName) ;
-    System.out.println(twoDArray.getClass().getName) ;
-    System.out.println(integer.getClass().getName) ;
-    System.out.println(itsDouble.getClass().getName) ;
-    System.out.println(itsShort.getClass().getName) ;
-    System.out.println(itsByte.getClass().getName) ;
-    System.out.println(itsBoolean.getClass().getName) ;
+    System.out.println(array.getClass().getName()) ;
+    System.out.println(twoDArray.getClass().getName()) ;
+
+    // All these will produce error. 
+    
+  //  System.out.println(integer.getClass().getName) ;
+  //  System.out.println(itsDouble.getClass().getName) ;
+  //  System.out.println(itsShort.getClass().getName) ;
+  //  System.out.println(itsByte.getClass().getName) ;
+   // System.out.println(itsBoolean.getClass().getName) ;
 
   }
 }
