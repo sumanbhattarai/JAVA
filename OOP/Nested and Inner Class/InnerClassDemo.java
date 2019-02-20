267bcb14bc96d05027e73180108bfcfcb485b48a
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-20 15:46:48 +0545

    Modified

--- OOP/Nested and Inner Class/InnerClassDemo.java
+++ OOP/Nested and Inner Class/InnerClassDemo.java
@@ -1,3 +1,26 @@
+class Outer
+{
+  int outerX = 100 ;
+  void test()
+  {
+    Inner inner = new Inner() ;
+    inner.display();
+
+  }
+  class Inner
+  {
+    int y = 10 ;
+    void display()
+    {
+      System.out.println("Display :  outerX = " + outerX) ;
+    }
+  }
+
+ 
+
+}
+
+
 class InnerClassDemo
 {
   public static void main(String[] args)
