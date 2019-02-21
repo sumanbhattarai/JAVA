e63582b48c02b1fe8a0e4b21aca36011c9ba39f0
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-21 09:27:40 +0545

    Whole Program Changed

--- Exception Handling/OwnExceptionDemo.java
+++ Exception Handling/OwnExceptionDemo.java
@@ -1,38 +1,43 @@
-class MyException extends Exception
-               {
-                   private int detail;
-                   MyException(int a)
-                   {
-                       detail = a ;
-                   }
-                   public String toString()
-                   {
-                       return "MyException[" + detail + "]" ;
-                   }     
-               }
-// Note : Overriding of toString() is optional and also constructor is optional.
-                
 
-                class OwnExceptionDemo
-                {
-                    static void compute(int a) throws MyException
-                    {
-                        if(a>10)
-                        {
-                            throw new MyException(a);
-                        }
-                    }
+class InvalidAge extends Exception
+{
+    int age ;
+    InvalidAge(int age)
+    {
+        this.age = age ;
+    }
 
-                    public static void main(String[] args)
-                    {
-                        try
-                        {
-                            compute(1);
-                            compute(20);
-                        }
-                        catch (MyException e)
-                        {
-                            System.out.println("Caught " + e.getMessage());
-                        }
-                    }
-                }
+    public String toString()
+    {
+        return "People of age " + age + " are incapable to vote." ;
+    }
+
+}
+
+
+class OwnExceptionDemo
+{
+    static void ageCheck( int age) throws InvalidAge
+    {
+        if(age<18)
+        {
+            throw new InvalidAge(age) ;
+        }
+        else
+        {
+            System.out.println("You can Vote");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try
+        {
+            ageCheck(16);
+        }
+        catch(InvalidAge e )
+        {
+            System.out.println(" Message is : " + e );
+        }
+    }
+}
