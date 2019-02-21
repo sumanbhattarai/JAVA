9653854a6b55286775e7d43c65455635d4b16322
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-21 10:02:49 +0545

    Initial Commit

---
+++ Introduction/StringInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class StringInput
+{
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in) ;
+        String name = obj.next() ;
+        System.out.println(name);
+    }
+}
