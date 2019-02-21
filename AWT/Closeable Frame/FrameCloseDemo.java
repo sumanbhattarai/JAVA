57337ebcc4b3889a52113787dfaa688642539437
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-02-21 18:33:00 +0545

    Simplified Code

--- AWT/Closeable Frame/FrameCloseDemo.java
+++ AWT/Closeable Frame/FrameCloseDemo.java
@@ -7,8 +7,6 @@ public class FrameCloseDemo
     public static void main(String[] args) 
     {
         Frame frame = new Frame ("Close Operation Frame");
-        Label lbl = new Label("Testing Close Operation") ;
-        frame.add(lbl);
         frame.setSize(300,300);
         frame.setVisible(true);
         frame.addWindowListener( new WindowAdapter()
