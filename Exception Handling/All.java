a02bd695df9c10ca41e19f59f0facdde3bec9e50
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-03-29 15:04:26 +0545

    Initial Commit

--- Exception Handling/All.java
+++ Exception Handling/All.java
@@ -1,11 +1,12 @@
-class ThrowsDemo
+class All
 {
 
     static void throw1() throws IllegalAccessException
     {
         System.out.println("Inside throw1");
-        // throw new ArithmeticException();
         throw new IllegalAccessException();
+
+        
         
     }
     public static void main(String[] args) 
@@ -17,6 +18,12 @@ class ThrowsDemo
         catch(IllegalAccessException e)
         {
             System.out.println("Caught Illegal Exception");
+        }
+
+        finally
+        {
+
+        System.out.println("Have a good day !");
         }
         
 
