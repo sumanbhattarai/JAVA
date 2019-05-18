ef09e45faf631826a2d889deae40a76db7442337
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-05-18 19:51:49 +0545

    Initial Commit

---
+++ File Handling/InputString.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner ;
+
+class InputString
+{
+    public static void main(String[] args) {
+        Scanner obj = new Scanner ( System.in) ;
+        String data = obj.nextLine();
+        System.out.println("You Entered : " + data );
+    }
+}
