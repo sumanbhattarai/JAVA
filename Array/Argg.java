7c559c60f1bd3950616b3f80531820bbc15856a9
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-05-18 18:16:42 +0545

    Initial Commit

---
+++ Array/Argg.java
@@ -0,0 +1,15 @@
+
+class Argg
+{
+    public static void main(String[] args) {
+        int sum=0 ;
+        for(int i =0 ; i<args.length ; i++)
+        {
+            if(Integer.parseInt(args[i])%2 == 0)
+            {
+                sum = sum+ Integer.parseInt(args[i]) ;
+            }
+        }
+        System.out.println("Sum of Even numbers is : " + sum);
+    }
+}
