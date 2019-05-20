f1076ab455e46ab666fd39648b0a901923404d6d
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-05-20 12:53:35 +0545

    Initial Commit

--- AWT/Event Handling/KeyListenerExample.java
+++ AWT/Event Handling/KeyListenerExample.java
@@ -2,49 +2,47 @@ import java.awt.*;
 import java.awt.event.*;  
 
 
-public class KeyListenerExample extends Frame implements KeyListener{  
-    Label l;  
-    TextArea area;  
-    KeyListenerExample(){  
-          
-        l=new Label();  
-        l.setBounds(20,50,100,20);  
-        area=new TextArea();  
-        area.setBounds(20,80,300, 300);  
-        area.addKeyListener(this);  
-          
-        add(l);add(area);  
-        setSize(400,400);  
-        setLayout(null);  
-        setVisible(true); 
+public class KeyListenerExample extends Frame implements KeyListener
+{  
+    TextField t ;
+    Label l ;
+
+    KeyListenerExample()
+    {
+        t = new TextField(20);
+        l  = new Label("Effect will be seen here : ") ;
+        add(t) ;
+        add(l);
+
+        t.addKeyListener(this);
+
+        setVisible(true);
+        setLayout(new FlowLayout());
+        setSize(200,200);
+
+    }
+
+    public void keyPressed(KeyEvent e)
+    {
+        l.setText("Key is pressed");
+    }
+
+    public void keyTyped(KeyEvent e)
+    {
+        l.setText("Key is typed.");
         
-        addWindowListener(new MyWindowAdapter(this));
-    }  
-    public void keyPressed(KeyEvent e) {  
-        l.setText("Key Pressed");  
-    }  
-    public void keyReleased(KeyEvent e) {  
-        l.setText("Key Released");  
-    }  
-    public void keyTyped(KeyEvent e) {  
-        l.setText("Key Typed");  
-    }  
-  
-    public static void main(String[] args) {  
-         new KeyListenerExample();  
-    }  
-}  
+
+    }
+
+    public void keyReleased(KeyEvent e)
+    {
+        l.setText("Key is Released.");
+
+    }
+
+    public static void main(String[] args) {
+        new KeyListenerExample();
+    }
 
 
-class MyWindowAdapter extends WindowAdapter{
-	
-	KeyListenerExample obj = null;
-	
-	MyWindowAdapter(KeyListenerExample obj){
-		this.obj = obj;
-	}
-	
-	public void windowClosing(WindowEvent we){
-		obj.dispose();
-	}
 }
