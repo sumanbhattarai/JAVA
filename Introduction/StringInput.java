a8d6480bcadce576088df5aec1d1f33a46af3dc9
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-05-22 09:24:13 +0545

    Updated

--- Introduction/StringInput.java
+++ Introduction/StringInput.java
@@ -4,7 +4,7 @@ class StringInput
 {
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in) ;
-        String name = obj.next() ;
+        String name = obj.next(); // use obj.nextLine() for full name. 
         System.out.println(name);
     }
 }
