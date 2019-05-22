4a164ce01d99d5f49f4c27ff0693dd3f2b72f024
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-05-22 16:50:43 +0545

    Initial Commit

---
+++ Question Set Solution/2016 Spring/Q4a.java
@@ -0,0 +1,25 @@
+
+import java.awt.* ;
+import java.awt.event.* ;
+
+public class Q4a extends Frame
+{
+    Q4a()
+    {
+        setSize(600,600);
+        setVisible(true);
+        setLayout(new FlowLayout());
+        setTitle("Frame Title");
+        addWindowListener( new WindowAdapter()
+        {
+            public void windowClosing( WindowEvent we)
+            {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new Q4a();
+    }
+}
